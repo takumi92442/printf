6ce6db19bd8b0ba89cea8252e7f87352f551b8d2
Takumi Sato <taksato@c1r27s2.42tokyo.jp>
2023-09-17 13:24:32 +0900

    a

--- ft_printf.h
+++ ft_printf.h
@@ -3,30 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: takumi <takumi@student.42.fr>              +#+  +:+       +#+        */
+/*   By: taksato <taksato@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 13:18:48 by marvin            #+#    #+#             */
-/*   Updated: 2023/08/23 01:13:26 by takumi           ###   ########.fr       */
+/*   Updated: 2023/09/17 13:20:44 by taksato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef FT_PRINTF_H
+#ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-
+# include <inttypes.h>
 # include <stdarg.h>
 # include <unistd.h>
-# include <inttypes.h>
 
-
-int     ft_printf(const char *, ...);
-int base_c(char C);
-int ft_strlen(const char *S);
-int  base_s(const char *S);
-int base_ten(int number);
-int base_pointer(uintptr_t p);
-int  big_base_sixteen(unsigned int number);
-int  smallbase_sixteen(unsigned int number);
-int unsigned_base_ten(unsigned int number);
-int persent();
+int	ft_printf(const char *fmt, ...);
+int	base_c(char C);
+int	ft_strlen(const char *S);
+int	base_s(const char *S);
+int	base_ten(int number);
+int	base_pointer(uintptr_t p);
+int	big_base_sixteen(unsigned int number);
+int	smallbase_sixteen(unsigned int number);
+int	unsigned_base_ten(unsigned int number);
+int	persent(void);
 #endif
